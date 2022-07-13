6eabaf605d55a291d12ae4a38e0048c148a6b832
Seungho <hamsh2003@xosoft.co.kr>
2022-07-13 16:34:07 +0900

    솔루션 파일 추가

---
+++ cppAL/cppAL.cpp
@@ -0,0 +1,12 @@
+﻿// cppAL.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	//	int num = GetRandom();
+	//nNum1 = GetRandom();
+	return 0;
+	//std::cout << "Hello World!\n";
+}
